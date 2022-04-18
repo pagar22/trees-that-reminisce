84792bde6c3fef21ee50c76ef50d92e8841cf86a
pagar22 <aaryanpagar@hotmail.com>
2022-04-18 23:35:11 +0200

    Generic Types (Int and String)

--- app/src/main/java/com/app/Driver.java
+++ app/src/main/java/com/app/Driver.java
@@ -5,20 +5,26 @@ import com.trees.AVLTree;
 public class Driver {
 
     public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
+        AVLTree<String> tree = new AVLTree<>();
 
-        tree.root = tree.insert(tree.root, 11);
-        tree.root = tree.insert(tree.root, 15);
-        tree.root = tree.insert(tree.root, 10);
-        tree.root = tree.insert(tree.root, 14);
-        tree.root = tree.insert(tree.root, 20);
-        tree.root = tree.insert(tree.root, 21);
-        tree.root = tree.insert(tree.root, 25);
+        tree.insert(tree.root, "Aaryan");
+        tree.insert(tree.root, "Saniya");
+        tree.insert(tree.root, "Diya");
+        //tree.insert(tree.root, 14);
+        //tree.insert(tree.root, 20);
+        //tree.insert(tree.root, 21);
+        //tree.insert(tree.root, 25);
 
-        System.out.println(tree.delete(tree.root, 15));
-        //tree.preOrder(tree.root);
         System.out.println(tree);
-        System.out.println(tree.search(tree.root, 15));
-        //15 11 10 21 20 25
+
+        System.out.println(tree.search(tree.root, "Aaryan"));
+        System.out.println(tree.delete(tree.root, "Aaryan"));
+        //System.out.println(tree.delete(tree.root, 10));
+        //System.out.println(tree.delete(tree.root, 13));
+        //tree.delete(tree.root, 14);
+        //tree.delete(tree.root, 20);
+
+        System.out.println(tree);
+        System.out.println(tree.search(tree.root, "Aaryan"));
     }
 }
--- app/src/main/java/com/trees/AVLTree.java
+++ app/src/main/java/com/trees/AVLTree.java
@@ -1,43 +1,31 @@
 package com.trees;
 
-public class AVLTree {
+public class AVLTree<Item> {
 
-    private class Node {
-        int key, height;
-        Node left, right;
+    public Node<Item> root;
 
-        public Node(int key) {
-            this.key = key;
-            this.height = 0;
-            this.left = this.right = null;
-        }
-
-        @Override
-        public String toString() {
-            return Integer.toString(this.key);
-        }
-    }
-
-    public Node root;
-
-    private int height(Node node) {
+    private int height(Node<Item> node) {
         return (node == null) ? -1 : node.height;
     }
 
-    private int balanceFactor(Node node) {
+    private int balanceFactor(Node<Item> node) {
         return (node == null) ? 0 : (height(node.left) - height(node.right));
     }
 
-    private void updateHeight(Node node) {
+    private void updateHeight(Node<Item> node) {
         node.height = 1 + Math.max(height(node.left), height(node.right));
     }
 
-    private Node findSmallest(Node node) {
+    private boolean isEmpty() {
+        return root == null;
+    }
+
+    private Node<Item> findSmallest(Node<Item> node) {
         while (node.left != null) node = node.left;
         return node;
     }
 
-    public String preOrder(Node node, StringBuilder builder) {
+    private String preOrder(Node<Item> node, StringBuilder builder) {
         if (node != null) {
             builder.append(node.key).append(" ");
             preOrder(node.left, builder);
@@ -47,9 +35,9 @@ public class AVLTree {
     }
 
     //Right Rotate
-    private Node rightRotate(Node y) {
-        Node x = y.left;
-        Node z = x.right;
+    private Node<Item> rightRotate(Node<Item> y) {
+        Node<Item> x = y.left;
+        Node<Item> z = x.right;
         x.right = y;
         y.left = z;
         updateHeight(y);
@@ -58,9 +46,9 @@ public class AVLTree {
     }
 
     //Left Rotate
-    private Node leftRotate(Node y) {
-        Node x = y.right;
-        Node z = x.left;
+    private Node<Item> leftRotate(Node<Item> y) {
+        Node<Item> x = y.right;
+        Node<Item> z = x.left;
         x.left = y;
         y.right = z;
         updateHeight(y);
@@ -68,20 +56,20 @@ public class AVLTree {
         return x;
     }
 
-    public Node balance(Node node, int key) {
+    public Node<Item> balance(Node<Item> node, Item key) {
 
         int balance = balanceFactor(node);
 
         //Left Heavy
         if (balance > 1) {
-            if (key > node.left.key)
+            if (node.left.compareTo(key) < 0)
                 node.left = leftRotate(node.left);
             return rightRotate(node);
         }
 
         //Right Heavy
         if (balance < -1) {
-            if(key < node.right.key)
+            if(node.right.compareTo(key) > 0)
                 node.right = rightRotate(node.right);
             return leftRotate(node);
         }
@@ -89,26 +77,26 @@ public class AVLTree {
         return node;
     }
 
-    public Node insert(Node node, int key) {
-        if (node == null) return new Node(key);
+    public Node<Item> insert(Node<Item> node, Item key) {
+        if (node == null) return root = new Node<>(key);
 
-        if (node.key > key) {
+        if (node.compareTo(key) > 0) {
             node.left = insert(node.left, key);
-        } else if(node.key < key) {
+        } else if(node.compareTo(key) < 0) {
             node.right = insert(node.right, key);
         } else throw new IllegalArgumentException("Cannot insert duplicate keys :(");
 
         updateHeight(node);
-        return balance(node, key);
+        return root = balance(node, key);
     }
 
-    public Node delete(Node node, int key) {
-        if (node == null) return node;
+    public Node<Item> delete(Node<Item> node, Item key) {
+        if (node == null) return null;
 
-        if (key > node.key)
-            node.right = delete(node.right, key);
-        else if (key < node.key)
+        if (node.compareTo(key) > 0)
             node.left = delete(node.left, key);
+        else if (node.compareTo(key) < 0)
+            node.right = delete(node.right, key);
         else {
             if (node.left == null && node.right == null)
                 node = null;
@@ -117,22 +105,23 @@ public class AVLTree {
             else if (node.right == null)
                 node = node.left;
             else {
-                Node successor = findSmallest(node.right);
+                Node<Item> successor = findSmallest(node.right);
                 node.key = successor.key;
                 node.right = delete(node.right, successor.key);
             }
         }
-        if (node == null) return node;
+
+        if (node == null) return null;
 
         updateHeight(node);
         return balance(node, key);
     }
 
-    public Node search(Node node, int key) {
-        if (node == null) return node;
+    public Node<Item> search(Node<Item> node, Item key) {
+        if (node == null) return null;
 
-        if (key < node.key) return search(node.left, key);
-        else if (key > node.key) return search(node.right, key);
+        if (node.compareTo(key) > 0) return search(node.left, key);
+        else if (node.compareTo(key) < 0) return search(node.right, key);
         else return node;
 
     }
@@ -140,7 +129,7 @@ public class AVLTree {
     @Override
     public String toString() {
 
-        if(root == null) throw new NullPointerException("Tree is Empty :(");
+        if (isEmpty()) throw new NullPointerException("Tree is Empty :(");
         StringBuilder builder = new StringBuilder(preOrder(root, new StringBuilder()))
                 .insert(0, "[")
                 .append("] ").append("[").append("H=").append(height(root)).append(" B=").append(balanceFactor(root)).append("]");
