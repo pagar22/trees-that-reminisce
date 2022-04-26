066193017321d68742b9e4f9408381d2dfe457a4
pagar22 <aaryanpagar@hotmail.com>
2022-04-26 19:37:31 +0200

    Cleaned ChangerGiver, fixed key-value bug

--- app/src/main/java/com/implementations/ChangeGiver.java
+++ app/src/main/java/com/implementations/ChangeGiver.java
@@ -3,52 +3,35 @@ package com.implementations;
 import com.trees.AVLTree;
 import com.trees.BinaryTreeBase;
 import com.trees.KeyValuePair;
-
-import java.util.ArrayList;
+import com.trees.Node;
 
 public class ChangeGiver {
 
 
-    public int minCoins (ArrayList<Integer> denoms, int amount, BinaryTreeBase<KeyValuePair<Integer, Integer>> memo) {
-        int minValue = amount;
+    public int pettyChange(int[] denoms, int amount, BinaryTreeBase<KeyValuePair<Integer, Integer>> calculated) {
+        int minCoins = amount;
 
-        if (denoms.contains(amount)) {
-            memo.insert(new KeyValuePair<>(1, amount));
-            //memo[amount] = 1;
+        //If there is a deno equal to amount, return 1 (only 1 deno needed)
+        for(int i : denoms) if(i==amount) {
+            calculated.insert(new KeyValuePair<>(1, amount));
             return 1;
         }
-        if ((memo.search(new KeyValuePair<>(amount)) != null))
-            return (memo.search(new KeyValuePair<>(amount))).key.value;
 
+        //If value is already calculated for this amount, don't calculate again
+        Node<KeyValuePair<Integer, Integer>> memo = calculated.search(new KeyValuePair<>(amount));
+        if (memo != null) return memo.key.key;
+
+        //Else go through all the denoms and find and store min required values recursively
         for(int i : denoms) {
-            int numValue = Integer.MAX_VALUE;
+            int numCoins = Integer.MAX_VALUE;
             if(i < amount)
-                numValue = 1 + minCoins(denoms, amount-i, memo);
-            if (numValue < minValue) {
-                minValue = numValue;
-                memo.insert(new KeyValuePair<>(minValue, amount));
-                //memo[amount] = minValue;
+                numCoins = 1 + pettyChange(denoms, amount-i, calculated);
+            if (numCoins < minCoins) {
+                minCoins = numCoins;
+                calculated.insert(new KeyValuePair<>(minCoins, amount));
             }
         }
-
-        return minValue;
+        return minCoins;
     }
 
-    public static void main(String[] args) {
-        ArrayList<Integer> denoms = new ArrayList<>();
-        denoms.add(1);
-        denoms.add(2);
-        denoms.add(3);
-        denoms.add(5);
-        int amount = 8;
-
-        /*int[] memo = new int[amount+1];
-        Arrays.fill(memo, 0);*/
-
-        AVLTree<KeyValuePair<Integer, Integer>> memo = new AVLTree<>();
-
-        ChangeGiver change = new ChangeGiver();
-        System.out.println(change.minCoins(denoms, amount, memo));
-
-    }
 }
