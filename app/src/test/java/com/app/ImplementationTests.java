a3905b5562d33f969da0a5ff615b5b1bd05b6210
pagar22 <aaryanpagar@hotmail.com>
2022-05-13 12:29:12 +0200

    Completed ImplementationTests using Injections

--- app/src/test/java/com/app/ImplementationTests.java
+++ app/src/test/java/com/app/ImplementationTests.java
@@ -1,6 +1,5 @@
 package com.app;
 
-import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
 import com.implementations.ChangeGiver;
@@ -9,7 +8,6 @@ import com.injectors.BinaryTreeInjector;
 import com.injectors.ChangeGiverInjector;
 import com.injectors.MazeInjector;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,7 +16,6 @@ public class ImplementationTests {
 
     @InjectMocks BinaryTreeInjector mazeInjector = new MazeInjector();
     @InjectMocks BinaryTreeInjector changeGiverInjector = new ChangeGiverInjector();
-    @Mock Maze maze = (Maze) mazeInjector.getBaseTreeInstance();
 
     @BeforeClass(alwaysRun = true)
     void testSetup() {
@@ -30,28 +27,12 @@ public class ImplementationTests {
 
     @Test(groups = {"implementation", "maze", "base"})
     public void mazeMockTestBaseTree() {
-        doThrow(new InstantiationError()).when(maze).setMaze(new char[][]{
-                {'x', 'x', 'x', 'x', 'x',},
-                {'x', ' ', 'x', ' ', 'x',},
-                {'x', ' ', ' ', ' ', 'x',},
-                {'x', ' ', 'x', ' ', 'x',},
-                {'x', 'x', 'x', ' ', 'x',},
-        });
-        boolean escaped = maze.escape(1, 1);
-        assertTrue(escaped);
+        assertTrue(executeMaze((Maze) mazeInjector.getBaseTreeInstance()));
     }
 
     @Test(groups = {"implementation", "maze", "avl"})
     public void mazeMockTestAVLTree() {
-        /*doReturn(true).when(maze).setMaze(new char[][]{
-                {'x', 'x', 'x', 'x', 'x',},
-                {'x', ' ', 'x', ' ', 'x',},
-                {'x', ' ', ' ', ' ', 'x',},
-                {'x', ' ', 'x', ' ', 'x',},
-                {'x', 'x', 'x', ' ', 'x',},
-        });
-        boolean escaped = maze.escape(1, 1);
-        assertTrue(escaped);*/
+        assertTrue(executeMaze((Maze) mazeInjector.getAVLTreeInstance()));
     }
 
     @Test(groups = {"implementation", "change_giver", "base"})
@@ -64,11 +45,22 @@ public class ImplementationTests {
         assertEquals(executeChangeGiver((ChangeGiver) changeGiverInjector.getAVLTreeInstance()), 4);
     }
 
+    private boolean executeMaze(Maze maze) {
+        //doReturn(true).when(maze).setMaze(new char[][]{
+        maze.setMaze(new char[][]{
+                {'x', 'x', 'x', 'x', 'x',},
+                {'x', ' ', 'x', ' ', 'x',},
+                {'x', ' ', ' ', ' ', 'x',},
+                {'x', ' ', 'x', ' ', 'x',},
+                {'x', 'x', 'x', ' ', 'x',},
+        });
+        return maze.escape(1, 1);
+    }
+
     private int executeChangeGiver(ChangeGiver changeGiver) {
         int[] denoms = {1, 2, 5, 10, 20, 50};
         int amount = 67;
         return changeGiver.pettyChange(denoms, amount);
     }
-
 
 }
